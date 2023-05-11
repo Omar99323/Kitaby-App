8563b00a48b07d26d1f8fecd2b8c0ace433dfcbb
Omar99323 <110248328+Omar99323@users.noreply.github.com>
2023-05-11 17:55:14 +0300

    sliding text animation

--- lib/core/widgets/splash_page_body.dart
+++ lib/core/widgets/splash_page_body.dart
@@ -1,8 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:kitaby/core/constants.dart';
 
-class SplashPageBody extends StatelessWidget {
+class SplashPageBody extends StatefulWidget {
   const SplashPageBody({super.key});
+
+  @override
+  State<SplashPageBody> createState() => _SplashPageBodyState();
+}
+
+class _SplashPageBodyState extends State<SplashPageBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animecon;
+  late Animation<Offset> anime;
+
+  @override
+  void initState() {
+    super.initState();
+    animecon = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        seconds: 1,
+      ),
+    );
+
+    anime = Tween<Offset>(
+      begin: const Offset(0, 5),
+      end: Offset.zero,
+    ).animate(animecon);
+
+    animecon.forward();
+  }
+
+  @override
+  // ignore: unnecessary_overrides
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,13 +47,23 @@ class SplashPageBody extends StatelessWidget {
         const SizedBox(
           height: 8,
         ),
-        const Text(
-          'Read free books',
-          style: TextStyle(
-            fontFamily: 'GT Sectra Fine',
-            fontSize: 20,
-          ),
-          textAlign: TextAlign.center,
+        // WIDGET TO BLUID ONLY ITS CHILD WHEN REBUILD
+        AnimatedBuilder(
+          animation: anime,
+          builder: (BuildContext context, Widget? child) {
+            // WIDGET TO INITIALIZE THE CHILD POSITION
+            return SlideTransition(
+              position: anime,
+              child: const Text(
+                'Read free books',
+                style: TextStyle(
+                  fontFamily: 'GT Sectra Fine',
+                  fontSize: 20,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            );
+          },
         ),
       ],
     );
